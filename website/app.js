763fdb667389bc5f312f6666ce87b5a55136c0c1
ahmedashouryounes <ahmedashouryounes@gmail.com>
2021-12-08 00:39:28 +0200

    add async get from openWeatherMap function

--- website/app.js
+++ website/app.js
@@ -1,5 +1,33 @@
 /* Global Variables */
-
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+// Personal API Key for OpenWeatherMap API
+const apiKey = "2bc7678332ff43cf91afd0e1686d7c21";
+// Event listener to add function to existing HTML DOM element
+document.getElementById('generate').addEventListener('click', generateAction);
+
+/* Function called by event listener */
+function generateAction(){
+    const clientZip = document.getElementById('zip').value;
+    const baseUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${clientZip}&appid=${apiKey}`;
+    getFromOpenWeatherMap(baseUrl)
+    .then((data)=>{
+        console.log(data)
+    })
+
+}
+
+/* Function to GET Web API Data*/
+const getFromOpenWeatherMap = async (url='') => {
+    const res = await fetch(url)
+    try {
+        const data = await res.json();
+        return data
+
+    } catch (error) {
+        console.log("error", error.message);
+    }
+}
+
